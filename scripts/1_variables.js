644146bbcb450b037f82ce2660a5bb14f11b3d10
Gaylord <lord.feza@gmail.fr>
2022-08-02 02:50:51 +0200

    Refactorisation code. New folders: public, scripts. New files:styleajax.css, variables.html. Rename of indexajax.html to index.html

---
+++ scripts/1_variables.js
@@ -0,0 +1,6 @@
+// Types : chaînes de caractères / String :
+console.log('Une chaîne de caractère avec "double guillemet"');
+
+// var textVar = "Du texte dans une variable appelée textVar";
+let textVar = "Du texte dans une variable appelée textVar";
+console.log(textVar);
---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+//Un commentaire
+
+/*Un commentaire
+        sur plusieurs 
+        lignes
+        */
+
+// consolo.log() aide à débugger, suivre les variables
+
+console.log("Mbote");
+console.dir(document);
+console.dir(document.body);
